3c39592dc2793bac27f701ac68c407ca3fbc8b6a
RipleyX <ripley_l@outlook.com>
2018-05-10 00:21:31 +0800

    入门示例代码，运行错误

--- app/src/main/java/com/example/ripley/amapapp/MainActivity.java
+++ app/src/main/java/com/example/ripley/amapapp/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.ripley.amapapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.MapView;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);//设置对应的XML布局文件
+
+        MapView mapView = findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);// 此方法必须重写
+        AMap aMap = mapView.getMap();
+
     }
 }
+
