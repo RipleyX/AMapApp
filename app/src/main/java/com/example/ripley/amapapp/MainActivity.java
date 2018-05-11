e4563e56bbc8fa68362bafa905f226c4d352adaa
RipleyX <ripley_l@outlook.com>
2018-05-11 23:18:07 +0800

    换成3D地图后直接黑屏了

--- app/src/main/java/com/example/ripley/amapapp/MainActivity.java
+++ app/src/main/java/com/example/ripley/amapapp/MainActivity.java
@@ -3,9 +3,9 @@ package com.example.ripley.amapapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.amap.api.maps2d.AMap;
-import com.amap.api.maps2d.MapView;
-import com.amap.api.maps2d.model.MyLocationStyle;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.MyLocationStyle;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -24,9 +24,9 @@ public class MainActivity extends AppCompatActivity {
         myLocationStyle.interval(2000); //设置连续定位间隔
         aMap.setMyLocationStyle(myLocationStyle); //设置定位蓝点的Style
         aMap.getUiSettings().setMyLocationButtonEnabled(true); //定位按钮
-        aMap.setMyLocationEnabled(true); //显示定位蓝点
-
-
+        aMap.setMyLocationEnabled(true); //打开定位图层
+        myLocationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);//连续定位、且将视角移动到地图中心点，定位点依照设备方向旋转，并且会跟随设备移动。（1秒1次定位）默认执行此种模式。
+        myLocationStyle.showMyLocation(true);  //显示定位蓝点
     }
     @Override
     protected void onDestroy() {
